295156dfe65673716d029893cd65c9b5f53a8607
maxy <maxy0619@gmail.com>
2024-06-27 07:29:28 -0400

    2nd commit adding azuredevops

--- obj/Debug/net8.0/myWebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/myWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e35b2cfa6e46f8dafcf2463ce239f0f502b81ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
